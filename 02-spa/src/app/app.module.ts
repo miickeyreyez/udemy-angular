d7a885ade734a5176f7329d19831ad4aec1b7f24
Angel Reyes <miickeyreyez@gmail.com>
2018-06-11 22:35:35 -0500

    Secciòn 04: RouterLink y animaciones para RouterLink

---
+++ 02-spa/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+import { APP_ROUTING } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
