475c8a2283427e559d4fdda0504ffcf1bcc718db
Angel Reyes <miickeyreyez@gmail.com>
2018-06-23 18:06:47 -0500

    Sección 7: Se crea la funcionalidad para agregar y eliminar elementos de la vista de agregar

--- 05-deseos/src/pages/agregar/agregar.component.ts
+++ 05-deseos/src/pages/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ListaDeseosService } from '../../app/services/lista-deseos.service';
+import { Lista, ListaItem } from '../../app/clases/index';
 
 @Component({
     selector:'app-agregar',
@@ -9,9 +10,29 @@ import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 export class AgregarComponent implements OnInit {
 
     nombreLista:string;
-    nombreItem:string;
+    nombreItem:string = "";
+
+    items:ListaItem[] = [];
 
     constructor(private listaDeseos:ListaDeseosService) {}
 
     ngOnInit(){}
+
+    agregar() {
+        if(this.nombreItem.length == 0) {
+            return;
+        }
+
+        let item = new ListaItem();
+        item.nombre = this.nombreItem;
+        this.items.push(item);
+        this.nombreItem = ""
+    }
+
+    borrar(i) {
+        console.log(i);
+        if(this.items.length > 0) {
+            this.items.splice(i,1);
+        }
+    }
 }
