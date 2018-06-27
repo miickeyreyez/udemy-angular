ad2ed3f413600d496da78508ff7a485b72b7b781
Angel Reyes <miickeyreyez@gmail.com>
2018-06-26 21:43:27 -0500

    Fin sección 7: Se termina la aplicación

--- 05-deseos/src/pages/terminado/terminado.component.ts
+++ 05-deseos/src/pages/terminado/terminado.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
+import { NavController } from 'ionic-angular';
+import { AgregarComponent } from '../agregar/agregar.component';
+import { Lista } from '../../app/clases';
+import { DetallesComponent } from '../detalles/detalles.component';
 
 @Component({
     selector:'app-terminado',
@@ -6,7 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TerminadoComponent implements OnInit {
-    constructor() {}
+    constructor(private listaDeseos:ListaDeseosService, private nav:NavController) {}
 
     ngOnInit(){}
+
+    verDetalle(lista:Lista, index:number){
+        //console.log(lista)
+        //console.log(index)
+        this.nav.push(DetallesComponent, {
+            lista, index
+        });
+        
+    }
 }
