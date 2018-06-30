d994c5940be0dd0e27acf3d5c3a65bf833506e9c
Miguel Angel Martinez Reyes <maxr@praxis.com.mx>
2018-06-30 17:41:38 -0500

    Sección 8:Se crean las rutas de los componentes en un archivo separado (rutas hijas)

--- 06-miscelaneos/src/app/app.routes.ts
+++ 06-miscelaneos/src/app/app.routes.ts
@@ -7,17 +7,19 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
 import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
 import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'usuario', 
+  { path: 'usuario/:id', 
     component: UsuarioComponent ,
-    children:[
+    children:USUARIO_ROUTES,
+    /*children:[
       { path: 'nuevo', component: UsuarioNuevoComponent },
       { path: 'editar', component: UsuarioEditarComponent },
       { path: 'detalles', component: UsuarioDetalleComponent },
       { path: '**', pathMatch: 'full', redirectTo:'nuevo' }
-    ]
+    ]*/
   },
   { path: '**', pathMatch: 'full', redirectTo:'home' }
 ];
--- 06-miscelaneos/src/app/components/usuario/usuario-nuevo.component.ts
+++ 06-miscelaneos/src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioNuevoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:ActivatedRoute) { 
+    this.router.parent.params.subscribe(params => {
+      //id = params['id'];
+    })
+  }
 
   ngOnInit() {
   }
