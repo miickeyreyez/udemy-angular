7c28a07c435178103c633863de1a010b597a5854
Miguel Angel Martinez Reyes <maxr@praxis.com.mx>
2018-07-02 19:02:03 -0500

    Sección 10: Validación por template o por controller

--- 08-formularios/src/app/app.module.ts
+++ 08-formularios/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './components/template/template.component';
@@ -15,7 +15,8 @@ import { DataComponent } from './components/data/data.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma:FormGroup;
+
+  constructor() {
+    this.forma = new FormGroup(
+      {
+        'nombre':new FormControl('', [Validators.required,Validators.minLength(3)]), 
+        'apellido':new FormControl('', Validators.required), 
+        'correo':new FormControl('', [Validators.required,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]), 
+      }
+    );
+   }
 
   ngOnInit() {
   }
 
+  guardarCambios() {
+    console.log(this.forma)
+  }
 }
