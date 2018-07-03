531be5d504e864e4be13df26941d19191389e596
Miguel Angel Martinez Reyes <maxr@praxis.com.mx>
2018-07-02 21:43:26 -0500

    Sección 10: Se muestra como crear formularios con objetos

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -9,12 +9,23 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataComponent implements OnInit {
 
   forma:FormGroup;
+  usuario:Object = {
+    nombrecompleto: {
+      nombre:"Angel",
+      apellido:"Reyes"
+    },
+    correo:"miickeyreyez@gmail.com"
+  }
 
   constructor() {
     this.forma = new FormGroup(
       {
+        'nombrecompleto':new FormGroup({
+          'nombre':new FormControl('', [Validators.required,Validators.minLength(3)]), 
+          'apellido':new FormControl('', Validators.required), 
+        }),
         'nombre':new FormControl('', [Validators.required,Validators.minLength(3)]), 
-        'apellido':new FormControl('', Validators.required), 
+        'apellido':new FormControl('', Validators.required),
         'correo':new FormControl('', [Validators.required,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]), 
       }
     );
