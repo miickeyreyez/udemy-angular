eb8b69ec8afddb28eb9ea6d6d4e4f76fdb39637a
Miguel Angel Martinez Reyes <maxr@praxis.com.mx>
2018-07-02 21:55:55 -0500

    Sección 10: Se muestra el setting y el reset de forms

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -9,15 +9,29 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataComponent implements OnInit {
 
   forma:FormGroup;
-  usuario:Object = {
+  usuario:any = {
     nombrecompleto: {
       nombre:"Angel",
       apellido:"Reyes"
     },
+    nombre:"Miguel",
+    apellido:"Martinez",
     correo:"miickeyreyez@gmail.com"
   }
 
   constructor() {
+    /*this.forma = new FormGroup(
+      {
+        'nombrecompleto':new FormGroup({
+          'nombre':new FormControl(this.usuario.nombrecompleto.nombre, [Validators.required,Validators.minLength(3)]), 
+          'apellido':new FormControl(this.usuario.nombrecompleto.apellido, Validators.required), 
+        }),
+        'nombre':new FormControl('', [Validators.required,Validators.minLength(3)]), 
+        'apellido':new FormControl('', Validators.required),
+        'correo':new FormControl(this.usuario.correo, [Validators.required,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]), 
+      }
+    );*/
+
     this.forma = new FormGroup(
       {
         'nombrecompleto':new FormGroup({
@@ -29,6 +43,9 @@ export class DataComponent implements OnInit {
         'correo':new FormControl('', [Validators.required,Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]), 
       }
     );
+
+    this.forma.setValue(this.usuario);
+
    }
 
   ngOnInit() {
@@ -36,5 +53,15 @@ export class DataComponent implements OnInit {
 
   guardarCambios() {
     console.log(this.forma)
+    this.forma.controls['nombre'].setValue("");
+    this.forma.reset({
+      nombrecompleto: {
+        nombre:"",
+        apellido:""
+      },
+      nombre:"",
+      apellido:"",
+      correo:""
+    });
   }
 }
