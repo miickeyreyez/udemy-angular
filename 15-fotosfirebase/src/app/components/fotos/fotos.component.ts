1b3e6e99c2c4c2e16c3f97a9c79314bf8afd1ee5
Miguel Angel Martinez Reyes <maxr@praxis.com.mx>
2018-07-10 22:17:09 -0500

    Sección 18: Se muestran las imagenes qeu existen, en tarjetas, ya se almacena en firebase database

--- 15-fotosfirebase/src/app/components/fotos/fotos.component.ts
+++ 15-fotosfirebase/src/app/components/fotos/fotos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { CargaImagenesService } from '../../services/carga-imagenes.service';
+export interface Item {nombre:string; url:string;}
 
 @Component({
   selector: 'app-fotos',
@@ -7,7 +11,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotosComponent implements OnInit {
 
-  constructor() { }
+  items: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('img').valueChanges();
+  }
 
   ngOnInit() {
   }
--- 15-fotosfirebase/src/app/services/carga-imagenes.service.ts
+++ 15-fotosfirebase/src/app/services/carga-imagenes.service.ts
@@ -12,13 +12,13 @@ export class CargaImagenesService {
   private CARPETA_IMAGENES = 'img'
   items: Observable<any[]>;
   constructor(private db: AngularFirestore) {
-    //this.items = db.collection('items').valueChanges();
+    //this.items = this.db.collection('img').valueChanges();
   }
 
   private guardarImagen(imagen:{nombre:string, url:string}) {
     console.log(imagen)
-    /*this.db.collection(`/${ this.CARPETA_IMAGENES }`)
-            .add( imagen );*/
+    this.db.collection(`/${ this.CARPETA_IMAGENES }`)
+            .add( imagen );
   }
 
   cargarImagenesFirebase(imagenes:FileItem[]) {
@@ -29,22 +29,25 @@ export class CargaImagenesService {
       if(item.progreso >= 100) {
         continue;
       }
+      const referenciaImagen = storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`);
       const uploadTask:firebase.storage.UploadTask = 
-        storageRef.child(`${this.CARPETA_IMAGENES}/${item.nombreArchivo}`)
-        .put(item.archivo);
+        referenciaImagen.put(item.archivo);
         uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
           (snapshot: firebase.storage.UploadTaskSnapshot) => item.progreso = (snapshot.bytesTransferred/snapshot.totalBytes) * 100,
           (error) => console.log("Error al subir", error),
           () => {
-            console.log('Imagen cargada correctamente')
-            item.url = uploadTask.snapshot.downloadURL;
-            console.log(uploadTask)
-            console.log(uploadTask.snapshot)
-            item.estaSubiendo = false;
-            this.guardarImagen({
-              nombre:item.nombreArchivo,
-              url:item.url
-            })
+            referenciaImagen.getDownloadURL().then(
+              (urlImagen) => {
+                console.log('Imagen cargada correctamente')
+                item.url = urlImagen;
+                item.estaSubiendo = false;
+                this.guardarImagen({
+                  nombre:item.nombreArchivo,
+                  url:item.url
+                })
+              },
+              (error) => console.log("Error: " + error)
+            )
           }
 
         ) 
